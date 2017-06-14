7c9ff47fb7610164156190fc52ece781bcddda2e
hyc <hc8023@126.com>
2017-06-14 18:25:17 +0800

    add alias shell

---
+++ alias.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+function appendAlias(){
+        bashrc=~/.bashrc
+        echo "alias l='ls -CF'" >> $bashrc
+        echo "alias la='ls -A'" >> $bashrc
+        echo "alias ll='ls -alF'" >> $bashrc
+        echo "alias ls='ls --color=auto'" >> $bashrc
+        echo "alias rm='rm -i'" >> $bashrc
+        echo "alias cd='cdls(){ cd \$1; ls; };cdls'" >> $bashrc
+        source $bashrc
+}
+appendAlias
