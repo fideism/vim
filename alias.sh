980ee64c16594e4cb3eaf2312cb3221eadfb399c
nignog <nignog@126.com>
2019-12-09 16:50:27 +0800

    remove git

--- alias.sh
+++ alias.sh
@@ -14,18 +14,6 @@ function appendAlias(){
         echo "alias gdiff='git diff .'">>$bashrc
         echo "alias composerupdate='composer update --profile'">>$bashrc
         echo "export TERM=xterm" >> $bashrc
-        echo "">>$bashrc
-        echo "#show git branch">>$bashrc
-        echo "function git_branch {" >> $bashrc
-        echo "  branch=\"\`git branch 2>/dev/null | grep \"^\\*\" | sed -e \"s/^\\*\\ //\"\`\"" >> $bashrc
-        echo "  if [ \"\${branch}\" != \"\"  ];then">>$bashrc
-        echo "           if [ \"\${branch}\" = \"(no branch)\"  ];then">>$bashrc
-        echo "                    branch=\"(\`git rev-parse --short HEAD\`...)\"">>$bashrc
-        echo "            fi">>$bashrc
-        echo "                    echo \" (\$branch)\"">>$bashrc
-        echo "  fi">>$bashrc
-        echo "}">>$bashrc
-        echo "export PS1='\u@\h \[\033[01;36m\]\W\[\033[01;32m\]\$(git_branch)\[\033[00m\] \\$ '">>$bashrc
         source $bashrc
 }
 appendAlias
--- git.sh
+++ git.sh
@@ -4,14 +4,6 @@ git config --global color.diff auto
 git config --global color.branch auto
 git config --global color.interactive auto
 git config --global credential.helper store
-git config --global user.name nigno
+git config --global user.name nignog
 git config --global user.mail nignog@126.com
 git config --global user.email nignog@126.com
-
-
-function completion(){
-       cp ./git/git-completion.sh /etc/profile.d/git-completion.sh
-       cp ./git/git-prompt.sh /etc/profile.d/git-prompt.sh
-       cp ./git/git.sh /etc/profile.d/git.sh
-}
-completion
