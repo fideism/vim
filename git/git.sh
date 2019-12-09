481d408f79d891cc5a4a93358d5e3d0221e51a5b
nigno <nignog@126.com>
2019-12-09 15:43:34 +0800

    git

---
+++ git/git.sh
@@ -0,0 +1,5 @@
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWSTASHSTATE=1
+export GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_SHOWUPSTREAM="verbose git svn"
+PS1='\[\033[1;32m\]\u@\h \[\033[1;34m\]\W\[\033[1;31m\]$(__git_ps1 " (%s)")\[\033[1;35m\] $ \[\033[0m\]'
